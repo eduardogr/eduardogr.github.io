8f493c65dfb2a68a8b46e86856ac9f9b38b7ace1
egarcia <egarcia@tuenti.com>
2019-10-06 16:23:23 +0200

    Updates

---
+++ precache-manifest.8f77d1eb3b233ffce24294ed29a4011e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f419db699b248cdc8ba6a453947ad5b",
+    "url": "/eduardogr.github.io/index.html"
+  },
+  {
+    "revision": "69e3e67d6968c9c23809",
+    "url": "/eduardogr.github.io/static/js/2.c50beab5.chunk.js"
+  },
+  {
+    "revision": "9d70f15cd1586c4dad0f",
+    "url": "/eduardogr.github.io/static/js/main.c3d83076.chunk.js"
+  },
+  {
+    "revision": "c12670efcb55f4bff02a",
+    "url": "/eduardogr.github.io/static/js/runtime-main.d237692e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/eduardogr.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
