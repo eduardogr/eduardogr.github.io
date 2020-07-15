43fc905706d6b9d0fbfcf2ffabd03ca4cdeb0e8b
Eduardo <garciaruiz.edu@gmail.com>
2020-07-15 14:38:52 +0200

    Adding a simple main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import injectSheet from 'react-jss'
-import logo from './logo.svg';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+
+import { profileAvatar } from '../resources'
 
 const stylesheet = {    
     'app': {
-        'textAlign': 'center'
+        'textAlign': 'center',
     },
 
     'appLogo': {
@@ -13,8 +17,8 @@ const stylesheet = {
     },
 
     'appHeader': {
-        'backgroundColor': '#282c34',
-        'minHeight': '100vh',
+        'backgroundColor': '#ffecba',
+        'minHeight': '5vh',
         'display': 'flex',
         'flexDirection': 'column',
         'alignItems': 'center',
@@ -23,32 +27,56 @@ const stylesheet = {
         'color': 'white'
     },
 
+    'main': {
+      'backgroundColor': '#ffecba',
+      'minHeight': '95vh',
+      'display': 'flex',
+      'flexDirection': 'column',
+      'alignItems': 'center',
+      'justifyContent': 'center',
+      'fontSize': 'calc(10px + 2vmin)',
+      'color': '#b35718'
+    },
+
     'appLink': {
         'color': '#61dafb'
     }
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  large: {
+    width: theme.spacing(45),
+    height: theme.spacing(45),
+  },
+}));
+
 const App = ({
     classes,
 }) => {
-    return (
-        <div className={classes.app}>
-          <header className={classes.appHeader}>
-            <img src={logo} className={classes.appLogo} alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className={classes.appLink}
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+  const moreClasses = useStyles();
+
+  return (
+      <div className={classes.app}>
+        <header className={classes.appHeader}>
+        </header>
+        <div className={classes.main}>
+          <Avatar src={profileAvatar} className={moreClasses.large} alt="profile" />
+          <p>
+            Hi! This will be my web page soon!
+          </p>
         </div>
-      );
+      </div>
+    );
 };
 
 export default injectSheet(stylesheet)(App);
---
+++ src/resources/index.js
@@ -0,0 +1,2 @@
+
+export const profileAvatar = "https://avatars0.githubusercontent.com/u/8736983?s=460&u=a15e0743590063392e16e68abaa3c06d00f066d9&v=4"
